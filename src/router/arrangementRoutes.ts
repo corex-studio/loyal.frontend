dd61ec429575916c0f4814a61b8bfc6fa6465e63
kennard <kyaxery@gmail.com>
2024-03-21 21:03:45 +0200

    fix

--- src/router/arrangementRoutes.ts
+++ src/router/arrangementRoutes.ts
@@ -1,5 +1,4 @@
 import { RouteRecordRaw } from 'vue-router'
-import { cloneDeep } from 'lodash'
 
 export const arrangementRoutes: RouteRecordRaw[] = [
   {
@@ -25,6 +24,26 @@ export const arrangementRoutes: RouteRecordRaw[] = [
   },
 ]
 
-export const arrangementRoutesInQrMenu = cloneDeep(arrangementRoutes)
-arrangementRoutesInQrMenu[0].path = 'qr_menu/arrangement'
-arrangementRoutesInQrMenu[0].name = 'qrMenuArrangementPage'
+export const arrangementRoutesInQrMenu: RouteRecordRaw[] = [
+  {
+    path: 'qr_menu/arrangement',
+    name: 'qrMenuArrangementPage',
+    component: () => import('src/pages/arrangement/ArrangementPage.vue'),
+    redirect: {
+      name: 'qrMenuOrderingPage',
+    },
+    children: [
+      {
+        path: 'ordering',
+        name: 'qrMenuOrderingPage',
+        component: () =>
+          import('src/pages/arrangement/ArrangementOrdering.vue'),
+      },
+      {
+        path: 'success_order/:orderId',
+        name: 'successOrderPage',
+        component: () => import('src/pages/arrangement/ArrangementSuccess.vue'),
+      },
+    ],
+  },
+]
--- src/services/eventBusKeys.ts
+++ src/services/eventBusKeys.ts
@@ -1,7 +1,6 @@
 import { EventBusKey } from '@vueuse/core'
 import { Company } from 'src/models/company/company'
 import { Order } from 'src/models/order/order'
-import { orderRepo } from 'src/models/order/orderRepo'
 
 export const selectCompanyKey: EventBusKey<{ company: Company }> =
   Symbol('selectCompanyKey')
