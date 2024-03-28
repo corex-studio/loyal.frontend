313f5f99498e8a4e936febf8a9044608f3232dc0
kennard <kyaxery@gmail.com>
2024-03-28 19:53:36 +0200

    added try catch for crypto (required for access from localhost by ip)

--- src/services/authKeyManager.ts
+++ src/services/authKeyManager.ts
@@ -11,10 +11,16 @@ class AuthHelper {
       .replaceAll(':', '')
     const token =
       'HIio68cvfayezE4dH6Yu0!nG=-3CpY0p?No7pLaCfEmxmmWkNcoSbaZ3bEOXhOU2tuCWAm!/iFOZG17gmcMXo?O5QiSEasbeKQ-h!zcDiW!u?54dze1L?X?NUj=gvlTWoatklgPi0u3!JbUly2ljZ7X3834pPFhUeygUmXflHU=aG-qK6qsL8jf2om2VywtxMo79B/WaJuc2yWFYX?R4t4mSZ5XFPHQ5MaAG2Crsu6swb7!VM8LaNPwglOT2130W'
-    const msgBuffer = new TextEncoder().encode(time + token)
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
-    const hashArray = Array.from(new Uint8Array(hashBuffer))
-    return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+    try{
+      const msgBuffer = new TextEncoder().encode(time + token)
+      const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer)
+      const hashArray = Array.from(new Uint8Array(hashBuffer))
+      return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('')
+    } catch (e) {
+      console.log(e)
+      return ''
+    }
+
   }
 
   async getFingerPrint() {
