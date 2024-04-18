1a83e17951187a087a64f44da3cc5a5b561dc3a9
msim <mskulkin35@gmail.com>
2024-04-18 16:46:38 +0200

    edit meta

--- src/models/metaTags/metaTags.ts
+++ src/models/metaTags/metaTags.ts
@@ -1,15 +1,25 @@
 import { useMeta } from 'quasar'
+import { companyGroupRepo } from '../companyGroup/companyGroupRepo'
 
 export const setMeta = (initialMeta?: Record<string, any>) => {
+  const siteData = localStorage.getItem('siteDataResponse')
+  let siteName = null
+  let siteDescription = null
+  if (siteData) {
+    siteName = JSON.parse(siteData).title
+    siteDescription = JSON.parse(siteData).description
+  }
   const metaData = {
-    title: initialMeta?.title ? String(initialMeta?.title) : 'Loyalhub',
+    title: initialMeta?.title
+      ? String(initialMeta?.title)
+      : siteName || companyGroupRepo.item?.name || 'Loyalhub',
     titleTemplate: (title: any) => `${title}`,
     meta: {
       description: {
         name: 'description',
-        content: initialMeta?.description || '',
+        content: initialMeta?.description || siteDescription || '',
       },
-      keywords: { name: 'keywords', content: initialMeta?.keywords },
+      keywords: { name: 'keywords', content: initialMeta?.keywords || '' },
     },
   }
 
