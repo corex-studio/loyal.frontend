5d0d981f8008f925b5ba2ae78400bcc0a21d820f
shmyaks <mskulkin35@gmail.com>
2024-06-06 16:03:33 +0200

    work with order statuses

---
+++ src/components/icons/CustomIcon.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-html="svg" class="icon" ref="parent"></div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+
+const props = defineProps<{
+  svg: string
+  height?: string
+  width?: string
+  color?: string
+}>()
+
+const parent = ref<HTMLDivElement>()
+
+const height = ref<string>('30px')
+const width = ref<string>('30px')
+const color = ref<string>('#000000')
+
+onMounted(() => {
+  props.height ? (height.value = props.height) : void 0
+  props.width ? (width.value = props.width) : void 0
+  props.color ? (color.value = props.color) : void 0
+
+  parent.value?.style.setProperty('--height', height.value)
+  parent.value?.style.setProperty('--width', width.value)
+  parent.value?.style.setProperty('--color', color.value)
+  parent.value?.style.setProperty('height', height.value)
+  // parent.value?.style.setProperty('width', width.value)
+})
+</script>
+
+<style lang="scss" scoped>
+.icon :deep(svg) * {
+  fill: var(--color);
+}
+
+.icon :deep(svg) {
+  height: var(--height);
+  width: var(--width);
+}
+</style>
