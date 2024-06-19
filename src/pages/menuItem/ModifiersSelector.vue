68a79b5a9011d9be3b5f46338db041865cab85a1
shmyaks <mskulkin35@gmail.com>
2024-06-19 16:47:21 +0200

    edited work with modifiers

--- src/pages/menuItem/ModifiersSelector.vue
+++ src/pages/menuItem/ModifiersSelector.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="row">
     <div
-      style="min-height: 36px"
       class="row no-wrap justify-between full-width mb-4"
+      style="min-height: 36px"
     >
       <div class="header3 bold col-8 ellipsis-2-lines">
-        {{ group.name
+        {{
+          group.name
         }}<span
-          v-if="
+        v-if="
             group.restrictions?.min_quantity &&
             !sum(group.items.map((el) => el.quantity))
           "
-          class="text-primary"
-        >
+        class="text-primary"
+      >
           *</span
-        >
+      >
       </div>
       <q-chip
         v-if="
@@ -32,18 +33,18 @@
         v-if="group.restrictions && group.restrictions.max_quantity > 1"
         class="row justify-between mb-10"
       >
-        <div style="opacity: 0.5" class="bold">
+        <div class="bold" style="opacity: 0.5">
           Добавлено: {{ sum(group.items.map((v) => v.quantity)) }} /{{
             group.restrictions?.max_quantity
           }}
         </div>
         <CIcon
           v-if="group.items.some((v) => v.quantity)"
-          @click="clearSelection()"
           class="cursor-pointer"
           color="on-background-color"
           hover-color="primary"
           name="fa-light fa-trash"
+          @click="clearSelection()"
         />
       </div>
       <div class="column">
@@ -51,38 +52,38 @@
           <ModifierRadioSelectorRow
             v-for="(el, index) in currentModifiers"
             :key="index"
-            @select="radioClickHandler($event)"
             :item="el"
+            @select="radioClickHandler($event)"
           />
         </template>
         <template v-if="modifierViewType === 'check'">
           <ModifierCheckSelectorRow
             v-for="(el, index) in currentModifiers"
             :key="index"
-            @select="checkboxClickHandler($event)"
             :item="el"
+            @select="checkboxClickHandler($event)"
           />
         </template>
         <template v-if="modifierViewType === 'row'">
           <ModifierSelectorRow
             v-for="(el, index) in currentModifiers"
             :key="index"
-            :item="el"
             :group="group"
+            :item="el"
           />
         </template>
         <div v-if="modifierViewType === 'card'" class="row full-width">
           <GridContainer
             :items="currentModifiers"
             :lg="3"
-            :xl="3"
             :md="3"
             :sm="2"
+            :xl="3"
             :xs="2"
             gap="20px"
           >
             <template v-slot:item="{ item }: { item: MenuModifierGroupItem }">
-              <OptionalModifierCard :group="group" :modifier="item" />
+              <OptionalModifierCard :group="group" :modifier="item"/>
             </template>
           </GridContainer>
         </div>
@@ -98,11 +99,11 @@ import {
 } from 'src/models/menu/menuModifierGroup//menuModifierGroup'
 import GridContainer from 'src/components/containers/GridContainer.vue'
 import OptionalModifierCard from './OptionalModifierCard.vue'
-import { computed } from 'vue'
+import {computed} from 'vue'
 import ModifierRadioSelectorRow from './ModifierRadioSelectorRow.vue'
 import ModifierCheckSelectorRow from './ModifierCheckSelectorRow.vue'
 import ModifierSelectorRow from './ModifierSelectorRow.vue'
-import { sum } from 'lodash'
+import {sum} from 'lodash'
 
 const props = defineProps<{
   group: MenuModifierGroup
@@ -144,12 +145,17 @@ const checkboxClickHandler = (item: MenuModifierGroupItem) => {
     if (
       props.group.restrictions &&
       sum(props.group.items.map((v) => v.quantity)) <
-        props.group.restrictions?.max_quantity
+      props.group.restrictions?.max_quantity
     )
       item.quantity = 1
     else return
   } else {
-    item.quantity = 0
+    if (!props.group.restrictions?.min_quantity) {
+      item.quantity = 0
+    } else if (sum(props.group.items.map((v) => v.quantity)) <= props.group.restrictions?.min_quantity) {
+      return;
+    } else
+      item.quantity = 0
   }
 }
 
