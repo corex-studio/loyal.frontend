f740df325f96e46d19d479d6b680b1ca3b2b1655
shmyaks <mskulkin35@gmail.com>
2024-06-26 16:22:29 +0200

    fixed menuItemModal image on loading

--- src/models/store.ts
+++ src/models/store.ts
@@ -16,6 +16,7 @@ import { Image } from './image/image'
 import { useWebSocket, UseWebSocketOptions, UseWebSocketReturn } from '@vueuse/core'
 import { MaybeRefOrGetter } from '@vueuse/shared'
 import { openWebsocket } from 'src/services/openWebsocket'
+import { MenuItem } from 'src/models/menu/menuItem/menuItem';
 
 type DeliveryAreaInfoDrawerData = {
   salesPoint: SalesPoint
@@ -62,9 +63,15 @@ export class Store {
   initialMenuItem: string | null = null
   menuItemImage: Image | null = null
   groupDragged = false
-  private _menuItemModal = false
   webSocket: UseWebSocketReturn<any> | null = null
+
+  private _menuItemModal = false
+
   // cityFromParam: string | null = null
+
+  get menuItemModal() {
+    return this._menuItemModal
+  }
 
   setWebSocket(
     url: MaybeRefOrGetter<string | URL | undefined>,
@@ -74,17 +81,15 @@ export class Store {
     return this.webSocket as unknown as UnwrapRef<ReturnType<typeof useWebSocket>>
   }
 
-  get menuItemModal() {
-    return this._menuItemModal
-  }
-
-  openMenuItemModal() {
+  openMenuItemModal(item?: MenuItem) {
     store.scrollPositionBeforeOpenProductModal = window.scrollY
+    store.menuItemImage = item?.image || null
     this._menuItemModal = true
   }
 
   closeMenuItemModal() {
     this._menuItemModal = false
+    store.menuItemImage = null
   }
 
   getCompanyGroup(externalId: string) {
@@ -150,11 +155,11 @@ export const addHash = (v: string) => {
 }
 
 export const totalDayTimes = () => {
-  return Array.from({ length: 24 }, (_, i) => i).reduce((r: string[], hour) => {
-    r.push(moment({ hour, minute: 0 }).format('HH:mm'))
-    r.push(moment({ hour, minute: 15 }).format('HH:mm'))
-    r.push(moment({ hour, minute: 30 }).format('HH:mm'))
-    r.push(moment({ hour, minute: 45 }).format('HH:mm'))
+  return Array.from({length: 24}, (_, i) => i).reduce((r: string[], hour) => {
+    r.push(moment({hour, minute: 0}).format('HH:mm'))
+    r.push(moment({hour, minute: 15}).format('HH:mm'))
+    r.push(moment({hour, minute: 30}).format('HH:mm'))
+    r.push(moment({hour, minute: 45}).format('HH:mm'))
 
     return r
   }, [])
