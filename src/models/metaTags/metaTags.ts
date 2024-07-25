eb0dd5c2b1943660c2740900f10ac3507daab748
kennardy <kirill.lehtman189@gmail.com>
2024-07-25 14:55:37 +0200

    fix

--- src/models/metaTags/metaTags.ts
+++ src/models/metaTags/metaTags.ts
@@ -7,10 +7,14 @@ export const setMeta = (initialMeta?: Record<string, any>) => {
   const siteData = localStorage.getItem('siteDataResponse')
   let siteName = currentCompany?.name || null
   let siteDescription = currentCompany?.description || null
-  if ((siteData && !currentCompany) || (siteData && !companyGroupRepo.item?.companies.length)) {
+  if (siteData) {
     siteName = JSON.parse(siteData).title
     siteDescription = JSON.parse(siteData).description
   }
+  // if ((siteData && !currentCompany) || (siteData && !companyGroupRepo.item?.companies.length)) {
+  //   siteName = JSON.parse(siteData).title
+  //   siteDescription = JSON.parse(siteData).description
+  // }
   const metaData: MetaOptions = {
     title: initialMeta?.title
       ? String(initialMeta?.title)
