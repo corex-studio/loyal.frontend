b32b3470bcdf9d068c2f32de21428270c1a3ca8e
msim <mskulkin35@gmail.com>
2024-09-27 17:40:34 +0300

    fix

--- src/layouts/footer/CFooter.vue
+++ src/layouts/footer/CFooter.vue
@@ -1,28 +1,28 @@
 <template>
   <div
     ref="footer"
-    class="bg-bottom-menu-color"
     :class="{ 'pb-30': $q.screen.lt.md && $store.tableMode }"
+    class="bg-bottom-menu-color"
   >
-    <q-separator v-if="!$q.screen.xs"/>
+    <q-separator v-if="!$q.screen.xs" color="divider-color" />
     <div class="pb-md-15 pb-xs-5" style="bottom: 0; z-index: 2100">
-      <div class="c-container" v-if="$q.screen.gt.sm">
+      <div v-if="$q.screen.gt.sm" class="c-container">
         <div class="footer-wrap row items-center pt-15">
-          <FooterDesktop/>
+          <FooterDesktop />
         </div>
       </div>
-      <div class="footer-wrap py-15 px-8 row" v-else>
-        <FooterMobile/>
+      <div v-else class="footer-wrap py-15 px-8 row">
+        <FooterMobile />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import {store} from 'src/models/store'
+<script lang="ts" setup>
+import { store } from 'src/models/store'
 import FooterDesktop from './FooterDesktop.vue'
 import FooterMobile from './FooterMobile.vue'
-import {onMounted, ref} from 'vue'
+import { onMounted, ref } from 'vue'
 
 const footer = ref<HTMLDivElement>()
 
@@ -36,7 +36,7 @@ onMounted(() => {
 })
 </script>
 
-<style scoped lang="scss">
+<style lang="scss" scoped>
 .q-footer {
   min-height: 196px;
 }
